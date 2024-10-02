b426e6bafe81d24f8b5ff63c81d815007ee911f0
Eli <elireid.28@gmail.com>
2024-10-02 18:23:39 -0400

    Created working form, Blazor style

---
+++ Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+public class RegisterModel
+{
+    [Required(ErrorMessage = "First Name is Required.")]
+    [StringLength(25, ErrorMessage = "First Name may not exceed 25 characters")]
+    public string FirstName { get; set;} = string.Empty;
+
+    [Required(ErrorMessage = "Last Name is Required.")]
+    public string LastName { get; set;} = string.Empty;
+
+    [Required(ErrorMessage = "Username is Required.")]
+    public string Username { get; set;} = string.Empty;
+
+    [Required(ErrorMessage = "Password is Required.")]
+    public string Password { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Confirmation Password is Required.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+   
+}
--- obj/Debug/net8.0/BlazorPractice.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8051e98bd0c71291128579cce340d2ed3e5fbbf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
